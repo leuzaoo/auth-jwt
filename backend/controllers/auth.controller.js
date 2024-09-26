3fd5aacd3e18b66fb44250b505dcc8b08e714bb8
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-26 09:49:44 -0300

    Created route to send verification email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
@@ -12,16 +12,11 @@ export const signup = async (req, res) => {
     }
 
     const userAlreadyExists = await User.findOne({ email });
+
     if (userAlreadyExists) {
       return res
         .status(400)
         .json({ success: false, message: "Este email já está em uso." });
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Email inválido." });
     }
 
     if (password.length < 6) {
@@ -60,13 +55,44 @@ export const signup = async (req, res) => {
     });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
-    console.log(
-      "Erro no controlador de criação de contas (signup): ",
-      error.message
-    );
-    res
-      .status(500)
-      .json({ success: false, message: "Erro no servidor interno." });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Código de verificação inválido ou expirado.",
+      });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+
+    await user.save();
+
+    await sendWelcomeEmail(user.email, user.name);
+
+    res.status(200).json({
+      success: true,
+      message: "Email verificado com sucesso.",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log(`Erro no controlador "verifyEmail": ${error}`);
+    res.status(500).json({ success: false, message: "Erro no servidor" });
   }
 };
 
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -22,3 +22,25 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${error}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "0c7c4a1f-6dc8-498c-8431-7c8f79d0fb16",
+      template_variables: {
+        company_info_name: "Park Massagem",
+        name: name,
+      },
+    });
+
+    console.log("Email de Boas Vindas enviado com sucesso", response);
+  } catch (error) {
+    console.error(`Erro ao tentar enviar o email de Boas Vindas: ${error}`);
+
+    throw new Error(`Erro ao tentar enviar o email de Boas Vindas: ${error}`);
+  }
+};
