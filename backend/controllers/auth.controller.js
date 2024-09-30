789570b000f062fe1cf2a2580db81ee3fffa1a17
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-30 08:09:38 -0300

    Fixed order to request to frontend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -10,7 +10,7 @@ import bcryptjs from "bcryptjs";
 import crypto from "crypto";
 
 export const signup = async (req, res) => {
-  const { email, password, name } = req.body;
+  const { name, email, password } = req.body;
 
   try {
     if (!email || !password || !name) {
