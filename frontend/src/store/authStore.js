6b1d0d3d0755133a4bf17d9f298719a549f63b3d
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-10-03 10:51:11 -0300

    Add dynamic values to development and production mode

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,10 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : "/api/auth";
 
 export const useAuthStore = create((set) => ({
   user: null,
